f08362a0ac1df7c3a0d860aba8363d080cd87e64
ayush113 <ayush.kumar1999@hotmail.com>
2019-02-05 11:05:17 +0530

    Added two test cases

---
+++ Phase-2/testcases/test-case-noerrors.c
@@ -0,0 +1,12 @@
+int min()
+{
+    int a,b;
+    int *c;
+
+    a= a - b;
+
+    while(a > 0)
+    {
+        a = a - 1;
+    }
+}
---
+++ Phase-2/testcases/test-case-paranthesis.c
@@ -0,0 +1,15 @@
+int main()
+{
+    int b;
+    int a = b + 5;
+
+    //printf("THIS IS A STRING");
+
+   
+    
+}
+
+// Missing Paranthesis for Function
+int func()
+{
+    int a  =  2 * 2;
